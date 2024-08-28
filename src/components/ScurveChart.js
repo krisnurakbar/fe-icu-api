c5db258ac342efb3176a68c5489148c178037c70
krisnurakbar <krisnurakbar@gmail.com>
2024-08-28 08:27:58 +0700

    add loading

--- src/components/ScurveChart.js
+++ src/components/ScurveChart.js
@@ -4,16 +4,33 @@ import { Line } from 'react-chartjs-2';
 
 function ScurveChart() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     axios.get('https://be-icu-api.vercel.app/api/project-progress')
       .then(response => {
         setData(response.data);
+        setLoading(false);
       })
       .catch(error => {
         console.error('Error fetching data:', error);
+        setError('Error fetching data');
+        setLoading(false);
       });
   }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (data.length === 0) {
+    return <div>No data available</div>;
+  }
 
   const chartData = {
     labels: data.map(item => `Week ${item.week_no}`),
